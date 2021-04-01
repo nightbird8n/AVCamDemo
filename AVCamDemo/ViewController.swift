d22f452c03dc21db45ec201dc0ae50d8b76d728e
Nami <nightbird8n@gmail.com>
2021-04-01 17:38:08 +0800

    init project

---
+++ AVCamDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVCamDemo
+//
+//  Created by Admin on 2021/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
