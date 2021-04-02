3e4376e151aaa7015e6d4c54cd772479e5919cf2
Nami <nightbird8n@gmail.com>
2021-04-02 20:28:36 +0800

    update

--- AVCamDemo/CamViewController.swift
+++ AVCamDemo/CamViewController.swift
@@ -6,13 +6,60 @@
 //
 
 import UIKit
+import SwiftyCam
 
-class CamViewController: UIViewController {
+class CamViewController: SwiftyCamViewController {
 
+    @IBOutlet weak var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cameraDelegate = self
+        switchButton.setImage(UIImage(systemName: "camera.rotate", withConfiguration: UIImage.SymbolConfiguration(pointSize: 27)), for: .normal)
+    }
+    
+    @IBAction func switchButtonClick(_ sender: Any) {
+        switchCamera()
     }
 
+}
+
+extension CamViewController: SwiftyCamViewControllerDelegate {
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+         // Called when takePhoto() is called or if a SwiftyCamButton initiates a tap gesture
+         // Returns a UIImage captured from the current session
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didBeginRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+         // Called when startVideoRecording() is called
+         // Called if a SwiftyCamButton begins a long press gesture
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+         // Called when stopVideoRecording() is called
+         // Called if a SwiftyCamButton ends a long press gesture
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishProcessVideoAt url: URL) {
+         // Called when stopVideoRecording() is called and the video is finished processing
+         // Returns a URL in the temporary directory where video is stored
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFocusAtPoint point: CGPoint) {
+         // Called when a user initiates a tap gesture on the preview layer
+         // Will only be called if tapToFocus = true
+         // Returns a CGPoint of the tap location on the preview layer
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didChangeZoomLevel zoom: CGFloat) {
+          // Called when a user initiates a pinch gesture on the preview layer
+          // Will only be called if pinchToZoomn = true
+          // Returns a CGFloat of the current zoom level
+    }
+
+    func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
+         // Called when user switches between cameras
+         // Returns current camera selection
+    }
+    
 }
